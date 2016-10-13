037ad3c002851d4a1a052082dd8849d7d518a9c6
Entrophius <Sondre.usn.hall@gmail.com>
2016-10-13 19:22:11 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
 $filename="student.txt";
 $fileaction="r";
+$matchcount =0;
 
 $file = fopen($filename,$fileaction);
 
@@ -12,15 +13,17 @@ example: if I enter "A1" in the input field on the html page it should return a 
 but if I enter A3 it should print all lines containing A3, and NOT any other lines. It will always be on the same place on each line, last. */
 
 while ( $linefromfile = fgets($file))
+if ($matchcount == 0) { print ("No matches"); }
 {
 
 $linefromfile = trim($linefromfile);
 
 $linefromfile = explode(";", $linefromfile);
 
-var_dump($linefromfile);
 
-if (empty($linefromfile)) {continue; }
+//var_dump($linefromfile);
+
+if (empty($linefromfile)) {continue;  }
 // classcode_input is the id of the input text field from the html page
 
 if (!empty($_POST['classcode_input'])) {
@@ -28,16 +31,20 @@ if ($_POST['classcode_input'] != $linefromfile[3]) {
 continue;
 }
 }
+  $matchcount++;
   print ("First name is $linefromfile[0]. ");
   print ("Last name is $linefromfile[1]. ");
   print ("Username is $linefromfile[2]. ");
   print ("Classcode is $linefromfile[3]");
   print ("<br>");
 }
+
+var_dump($matchcount);
 fclose($file);
 
-/* current output is: array(4) { [0]=> string(6) "Mangla" [1]=> string(5) "Surma" [2]=> string(5) "Bngli" [3]=> string(2) "A3" } First name is Mangla. Last name is Surma. Username is Bngli. Classcode is A3
-array(4) { [0]=> string(7) "Testone" [1]=> string(7) "Lastone" [2]=> string(2) "tl" [3]=> string(2) "A4" } array(4) { [0]=> string(4) "Link" [1]=> string(7) "Surname" [2]=> string(2) "ls" [3]=> string(2) "A3" } First name is Link. Last name is Surname. Username is ls. Classcode is A3
+/* current output is: No matchesNo matchesNo matches
+Notice: Undefined offset: 3 in C:\xampp\htdocs\Oblig1\test.php on line 30
 
+Fatal error: Cannot break/continue 1 level in C:\xampp\htdocs\Oblig1\test.php on line 31
 */
 ?>
