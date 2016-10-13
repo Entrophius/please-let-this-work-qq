0aeb8a28cfae69ddb3e6c7282e1bd90318c8c9a2
Entrophius <Sondre.usn.hall@gmail.com>
2016-10-13 21:35:45 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -13,24 +13,27 @@ $classcode = $_POST["classcode_input"];
 while ( $linefromfile = fgets($file))
 {
 
-// trim
+// jeg måtte bruke trim for å få det til å funke, litt usikkert hvorfor
 $linefromfile = trim($linefromfile);
 
 // explode linjene fra filen til array, ; som delimiter/skilletegn/breaktegn 
 $linefromfile = explode(";", $linefromfile);
 
+// hvis classcode (se input variablen) ikke matcher det siste (0->1->2->3) fortsetter den til den finner det
 if ($classcode != $linefromfile[3]) {
-continue;
-}
 
+// her har den funnet en match og hopper ut av den over, legger til en på match og printer
   $matchcount++;
   print ("First name is $linefromfile[0]. ");
   print ("Last name is $linefromfile[1]. ");
   print ("Username is $linefromfile[2]. ");
   print ("Classcode is $linefromfile[3]");
-  print ("<br>");
+  print ("<br>");;
 }
+}
+
 if ($matchcount == 0) { print ("No matches"); }
 //var_dump($matchcount);
 fclose($file);
+// current output: everything in file
 ?>
