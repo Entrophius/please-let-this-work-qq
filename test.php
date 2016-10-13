6278f891f2fa1b8217329b0942dcd1aa6ed88f9d
Entrophius <Sondre.usn.hall@gmail.com>
2016-10-13 18:02:31 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -6,6 +6,11 @@ $file = fopen($filename,$fileaction);
 
 var_dump($_POST);
 
+/* The intended behavior is for the print functions to only print the lines that contain a matching classcode
+which can be any combination of numbers and letters, but right now it's just A3 and A4
+example: if I enter "A1" in the input field on the html page it should return a message saying "No matches found"
+but if I enter A3 it should print all lines containing A3. It will always be on the same place on each line, last. */
+
 while ( $linefromfile = fgets($file))
 {
 
